cd02a225a7ec5688cd50ee1ad7b20cad7ed7bf23
Owen Schumacher <owenschu7@gmail.com>
2025-02-21 17:04:36 -0800

    new changes

--- src/todo.cpp
+++ src/todo.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include "display.h"
+#include <vector>
+#include <string>
 
 int main() {
 
+  //create new vector
+  std::vector<std::string> todoList;
+  
+  
+
+
+
+
   startDisplay();
   int input;
+  std::string strInput;
   while (true) {
     std::cin >> input;
     switch(input) {
       case(1):
-        std::cout << "You entered 1" << std::endl;
+	//add item to list
+	//prompt user for name of item
+	//append item to vector and store it in data.txt      
+        std::cout << "What would you like to add to the list" << std::endl;
+	std::cin.ignore();
+	std::getline(std::cin, strInput);
+	todoList.push_back(strInput);
         break;
       case(2):
-        std::cout << "You entered 2" << std::endl;
+	//remove item
+	//prompt user for name of item
+	//remove item from vector and data.txt
+	std::cout << "You entered 2" << std::endl;
+
         break;
       case(3): 
-        std::cout << "You entered 3" << std::endl;
+        //list all items
+	std::cout << "===== To-Do list =====" << std::endl;
+	for (std::string str : todoList) {
+          std::cout << " - " << str << std::endl;
+	}
+
+
+
         break;
       case(4):
         std::cout << "Exiting Program." << std::endl;
@@ -23,9 +51,6 @@ int main() {
       default:
 	break;
     }
-
-
-
   }
 
 
